3d37d163bd26521019d01403e2f98b27c6099700
Jhonni10 <jhonieripianoscue@gmail.com>
2024-10-01 10:41:05 -0500

    Added clsx dependency, updated TasksList component to display loading state, and modified tailwind.config.js to include new keyframes and animations.

--- frontend/src/components/TasksList.js
+++ frontend/src/components/TasksList.js
@@ -3,18 +3,30 @@
 import React, { useEffect, useState } from 'react'
 import { TaskCard } from './TaskCard'
 import { getAllTasks } from '@/api/api-tasks';
+import LoadingDots from './LoadingDots';
 
 export const TasksList = () => {
 
     const [tasks, setTasks] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
       async function loadTasks() {
+        setLoading(true);
         const res = await getAllTasks();
         setTasks(res.data);
+        setLoading(false);
       }
       loadTasks();
     }, []);
+
+    if (loading) {
+        return (
+          <div className="font-semibold text-lg text-center h-[calc(100vh-140px)] flex justify-center items-center bg ">
+            { loading && <LoadingDots className='bg-gray-400'/>}
+          </div>
+        );
+      }
 
     if (!tasks || tasks.length == 0) {
         return (
